b807c681fa908b866284caf61ba24e97408ddade
Roger Johansson <roger@asynkron.se>
2025-08-15 18:27:54 +0200

    Use test-specific vehicle data provider

--- TpVehicleAPI.Tests/VehicleEndpointTests.cs
+++ TpVehicleAPI.Tests/VehicleEndpointTests.cs
@@ -1,7 +1,9 @@
 using System.Net;
 using System.Net.Http.Json;
-using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using TpVehicleAPI;
 
 namespace TpVehicleAPI.Tests;
@@ -14,21 +16,26 @@ public class VehicleEndpointTests : IClassFixture<WebApplicationFactory<Program>
     public VehicleEndpointTests(WebApplicationFactory<Program> factory)
     {
         _client = factory.WithWebHostBuilder(builder =>
-            builder.UseEnvironment("Testing").UseKestrel()).CreateClient();
+            builder.UseEnvironment("Testing").UseKestrel()
+                .ConfigureServices(services =>
+                {
+                    services.RemoveAll<IVehicleDataProvider>();
+                    services.AddSingleton<IVehicleDataProvider, TestVehicleDataProvider>();
+                })).CreateClient();
     }
 
     [Fact]
     public async Task ReturnsVehicleForKnownRegistration()
     {
-        var vehicle = await _client.GetFromJsonAsync<VehicleResponse>("/vehicles/ABC123");
+        var vehicle = await _client.GetFromJsonAsync<VehicleResponse>("/vehicles/TST123");
         Assert.NotNull(vehicle);
-        Assert.Equal("ABC123", vehicle!.RegistrationNumber);
+        Assert.Equal("TST123", vehicle!.RegistrationNumber);
     }
 
     [Fact]
     public async Task ReturnsNotFoundForUnknownRegistration()
     {
-        var response = await _client.GetAsync("/vehicles/ZZZ999");
+        var response = await _client.GetAsync("/vehicles/NOPE999");
         Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
     }
 
